39c4236cffba00b444b3285a771053542227369d
simon <simon.florysiak@gmail.com>
2021-10-27 15:57:26 +0200

    create pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-
+import EmployeeListPage from "./pages/EmployeeListPage/EmployeeListPage";
+import CreateEmployeePage from "./pages/CreateEmployeePage/CreateEmployeePage";
 
 export default function App() {
   return (
     <Router>
-               
       <Switch>
-        <Route exact path="/"/>
-
+        <Route exact path="/" component={CreateEmployeePage}/>
+        <Route path="/list" component={EmployeeListPage}/>
       </Switch>
-      
     </Router>
   );
 }
